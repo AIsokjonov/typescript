443d545225469c3934754f30aa6c7268b5761393
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-09 16:07:23 +0900

    arrays

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var nums = [1, 2, 3, 4, 5];
+var nums2;
+nums2 = [243, 5435, 13, 543];
+var strings = ["strings", "array", "only"];
+console.log(nums);
+console.log(nums2);
+console.log(strings);
+console.log(typeof (nums));
+// generic array declaration
+var nums3 = [1, 2, 3, 4];
+console.log(nums3);
+console.log(typeof (nums3));
---
+++ arrays.ts
@@ -0,0 +1,16 @@
+let nums = [1,2,3,4,5];
+
+let nums2: number[];
+nums2 = [243,5435,13,543];
+
+let strings = ["strings", "array", "only"];
+
+console.log(nums);
+console.log(nums2);
+console.log(strings);
+console.log(typeof(nums));
+
+// generic array declaration
+let nums3: Array<number> = [1, 2,3,4]
+console.log(nums3);
+console.log(typeof(nums3));
