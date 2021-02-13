3ff84dba28d5721b402f8f1180b015a5792b4074
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-13 11:37:53 +0900

    more on unknown type

--- unknown.js
+++ unknown.js
@@ -1,11 +1,46 @@
+var _a;
+var var1 = "It is a string";
+console.log(var1.substring(0, 2));
+var1 = 1;
+// console.log(var1.substring(0,2)); throws an error because number does not have substring() method
+var var2 = "this is a string";
+var var2Str = var2;
+console.log(var2Str.substring(0, 4));
+;
+;
+;
+function printo(o) {
+    var _a;
+    if ((_a = o.m1) === null || _a === void 0 ? void 0 : _a.m2) {
+        console.log(o.m1.m2.m3);
+    }
+}
+;
+var obj1 = {
+    m1: undefined
+};
+var obj2 = {
+    m1: {
+        m2: undefined
+    }
+};
+var obj3 = {
+    m1: {
+        m2: {
+            m3: "Yeah, This is IT!"
+        }
+    }
+};
+printo(obj1);
+printo(obj2);
+printo(obj3);
+// nullish coalescing
 function getValue() {
     if (Math.random() > 0.5) {
         return undefined;
     }
     return "Good";
 }
-var value = getValue();
-if (!value) {
-    value = "Default";
-}
+;
+var value = (_a = getValue()) !== null && _a !== void 0 ? _a : "default";
 console.log(value);
--- unknown.ts
+++ unknown.ts
@@ -1,12 +1,54 @@
-function getValue(): string | undefined{
-  if (Math.random() > 0.5){
-    return undefined;
-  }
-  return "Good";
-}
+let var1: any = "It is a string";
+console.log(var1.substring(0,2));
+var1 = 1;
+// console.log(var1.substring(0,2)); throws an error because number does not have substring() method
 
-let value = getValue();
-if(!value){
-  value = "Default"
+let var2: unknown = "this is a string";
+let var2Str = var2 as string;
+console.log(var2Str.substring(0,4));
+
+interface ObjectC {
+	m3: string;
+};
+interface ObjectB {
+	m2?: ObjectC;
+};
+interface ObjectA {
+	m1?: ObjectB;
+};
+
+function printo(o: ObjectA): void {
+	if (o.m1?.m2) {
+		console.log(o.m1.m2.m3);
+	}
+};
+
+const obj1: ObjectA = {
+	m1: undefined,
 }
+const obj2: ObjectA = {
+	m1: {
+		m2: undefined,
+	},
+};
+const obj3: ObjectA = {
+	m1: {
+		m2: {
+			m3: "Yeah, This is IT!",
+		},
+	},
+};
+printo(obj1);
+printo(obj2);
+printo(obj3);
+
+// nullish coalescing
+function getValue(): string | undefined {
+	if (Math.random() > 0.5) {
+		return undefined;
+	}
+	return "Good";
+};
+
+let value = getValue() ?? "default";
 console.log(value);
