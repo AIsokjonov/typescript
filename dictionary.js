37bbb03854f52906c54902079d45be48040d4ab5
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-19 15:38:25 +0900

    dictionary

---
+++ dictionary.js
@@ -0,0 +1,9 @@
+var _a;
+;
+;
+var dict = (_a = {},
+    _a[1] = { id: 1, name: "Robert" },
+    _a[2] = { id: 2, name: "James" },
+    _a[3] = { id: 3, name: "Bob" },
+    _a);
+console.log(dict[2].name);
