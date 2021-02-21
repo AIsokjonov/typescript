f25a52a3aafd03788ac33293924a994dcfe4aaf7
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-21 23:07:50 +0900

    strongly typed tuples

---
+++ typedTuples.js
@@ -0,0 +1,5 @@
+var tomScore = ["Tom", 70];
+console.log(tomScore);
+// open ended tuples
+var t2 = ["Robert", 234, 12, 543, 754];
+console.log(t2);
---
+++ typedTuples.ts
@@ -0,0 +1,6 @@
+const tomScore: [string, number] = ["Tom", 70];
+console.log(tomScore);
+
+// open ended tuples
+const t2: [string, ...number[]] = ["Robert", 234,12,543,754];
+console.log(t2);
