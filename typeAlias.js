2889dcf90f4dd5af1362dfbc9885ca78cce912a6
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-21 23:14:17 +0900

    type aliasing

---
+++ typeAlias.js
@@ -0,0 +1,6 @@
+var tom = { name: "Tom", score: 90 };
+var bob = { name: "Bob", score: 89 };
+var robert = { name: "Robert", score: 70 };
+console.log("Tom's score: " + tom.score);
+console.log("Bob's score " + bob.score);
+console.log("Robert's score: " + robert.score);
---
+++ typeAlias.ts
@@ -0,0 +1,9 @@
+type Score = { name: string, score: number };
+
+const tom: Score = { name: "Tom", score: 90 };
+const bob: Score = { name: "Bob", score: 89 };
+const robert: Score = { name: "Robert", score: 70 };
+
+console.log(`Tom's score: ${tom.score}`);
+console.log(`Bob's score ${bob.score}`);
+console.log(`Robert's score: ${robert.score}`);
