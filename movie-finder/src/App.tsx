3e78ca8d11655ad86f8c6d095719958510d10e07
AIsokjonov <abdurauf.isokjonov@gmail.com>
2021-02-24 11:10:34 +0900

    changed the project

--- movie-finder/src/App.tsx
+++ movie-finder/src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useReducer } from 'react';
+
+type HNResponse = {
+  hits: { 
+    title: string;
+    objectID: string;
+    url: string;
+  }[]
+};
+
+type State =
+  | { status: 'empty' }
+  | { status: 'loading' }
+  | { status: 'error', error: string }
+  | { status: 'success', data: HNResponse };
+
+type Action =
+  | { kind: 'request' }
+  | { kind: 'success', results: HNResponse }
+  | { kind: 'failure', error: string };
+
+function reducer(state: State, action: Action): State {
+  switch (action.kind) {
+    case 'request':
+      return { status: 'loading' };
+    case 'success':
+      return { status: 'success', data: action.results };
+    case 'failure':
+      return { status: 'error', error: action.error };
+  }
+}
 
 function App() {
+  const [query, setQuery] = useState<string>();
+  const [state, dispatch] = useReducer(reducer, { status: 'empty' });
+
+  useEffect(() => {
+    let ignore = false;
+
+    dispatch({ kind: 'request' });
+    fetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        .then(
+            (response: Response) => response.json().then(
+                (results: HNResponse) => { 
+                    if (!ignore) dispatch({ kind: 'success', results }); 
+                }
+            ),
+            (error: Error) => dispatch({ kind: 'failure', error: error.message })
+        );
+
+    return () => { ignore = true; }
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={query} onChange={e => setQuery(e.target.value)} />
+      {state.status === 'loading' && <span>Loading...</span>}
+      {state.status === 'success' && <ul>
+        {state.data && state.data.hits && state.data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>}
+      {state.status === 'error' && <span>Error: {state.error}</span>}
     </div>
   );
 }
